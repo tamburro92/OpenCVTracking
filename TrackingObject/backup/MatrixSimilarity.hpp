37df0fb60e08d4cdd88a9a365875d39d7414bd26
tamburro92 <arturo.ingenito@hotmail.com>
2016-05-07 21:41:00 +0200

    .

---
+++ TrackingObject/backup/MatrixSimilarity.hpp
@@ -0,0 +1,119 @@
+#include <cfloat>
+#include <opencv2/core/core.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+using std::vector;
+
+#define DMAX 3
+#define MINVALUE -1000
+
+class MatrixSimilarity {
+private:
+public:
+
+	int rows;
+	int cols;
+	float** matrix;
+	std::vector<bool> isObjectDeleted;
+	std::vector<bool> isBlobsDeleted;
+
+	MatrixSimilarity(int r, int c) {
+		rows = r;
+		cols = c;
+		matrix = new float*[rows];
+		for (int i = 0; i < rows; i++)
+			matrix[i] = new float[cols];
+
+		for (int i = 0; i < rows; isBlobsDeleted.push_back(false), i++)
+			;
+		for (int i = 0; i < cols; isObjectDeleted.push_back(false), i++)
+			;
+		fillMatrixReset();
+	}
+	~MatrixSimilarity() {
+		for (int i = 0; i < rows; ++i)
+			delete[] matrix[i];
+		delete[] matrix;
+		rows = 0;
+		cols = 0;
+	}
+
+	bool deleteFromMatrix(int blob, int obj) {
+		if (blob > rows || obj > cols)
+			return false;
+		isBlobsDeleted[blob] = true;
+		isObjectDeleted[obj] = true;
+		return true;
+
+	}
+	void calculateMatrix(std::vector<Point2f> blobs,
+			std::vector<Point2f> objs) {
+		for (int i = 0; i < rows; i++)
+			for (int j = 0; j < cols; j++)
+				if (cv::norm(blobs[i] - objs[j]) < DMAX)
+					matrix[i][j] = 1 - cv::norm(blobs[i] - objs[j]) / DMAX;
+
+	}
+	std::vector<float> maxMatrix() {
+		std::vector<float> maxPoint;
+
+		float maxValue = MINVALUE;
+		int rowMax = -1;
+		int colMax = -1;
+		for (int i = 0; i < rows; i++) {
+			if (isBlobsDeleted[i])
+				continue;
+			for (int j = 0; j < cols; j++) {
+				if (isObjectDeleted[j])
+					continue;
+				if (matrix[i][j] > maxValue) {
+					maxValue = matrix[i][j];
+					rowMax = i;
+					colMax = j;
+				}
+
+			}
+		}
+		maxPoint.push_back(rowMax);
+		maxPoint.push_back(colMax);
+		maxPoint.push_back(maxValue);
+		return maxPoint;
+	}
+	std::vector<int> remainBlobs() {
+		std::vector<int> blobs;
+		for (int i = 0; i < rows; i++)
+			if (!isBlobsDeleted[i])
+				blobs.push_back(i);
+
+		return blobs;
+	}
+	std::vector<int> remainObjects() {
+		std::vector<int> obj;
+		for (int i = 0; i < cols; i++)
+			if (!isObjectDeleted[i])
+				obj.push_back(i);
+
+		return obj;
+	}
+	void fillMatrixReset() {
+		for (int i = 0; i < rows; i++)
+			for (int j = 0; j < cols; j++)
+				matrix[i][j] = 0;
+
+	}
+};
+
+ostream &operator<<(ostream &os, const MatrixSimilarity &matrix) {
+	for (int i = 0; i < matrix.rows; i++) {
+		for (int j = 0; j < matrix.cols; j++) {
+			if (matrix.isBlobsDeleted[i] || matrix.isObjectDeleted[j])
+				os << "D" << " ";
+			else
+				os << matrix.matrix[i][j] << " ";
+		}
+		os << endl;
+	}
+	return os;
+}
---
+++ TrackingObject/backup/main.cpp
@@ -0,0 +1,124 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/video/video.hpp>
+#include <iostream>
+#include "MatrixSimilarity.hpp"
+
+using namespace cv;
+using namespace std;
+
+
+#define MINAREA 120
+
+int main(int argc, char** argv) {
+	RNG rng(12345);
+	Mat frame;
+	Mat fgMaskMOG2, fgMaskKNN;
+	Mat out;
+	Mat kernelErosion = getStructuringElement(MORPH_RECT, Size(3, 3),
+			Point(-1, -1));
+	Mat kernelDilatation = getStructuringElement(MORPH_RECT, Size(3, 3),
+			Point(-1, -1));
+
+	int keyboard;
+
+	VideoCapture src("video.avi");
+
+	namedWindow("Frame");
+	namedWindow("FG Mask MOG 2");
+	moveWindow("FG Mask MOG 2", 0, 500);
+	namedWindow("FG Mask MOG 2 Filtered");
+	moveWindow("FG Mask MOG 2 Filtered", 500, 0);
+
+	if (!src.isOpened())
+		exit(EXIT_FAILURE);
+
+	Ptr<BackgroundSubtractor> backGroundSubMOG2 =
+			new BackgroundSubtractorMOG2();
+
+	while ((char) keyboard != 'q') {
+		if (!src.read(frame)) {
+			cout << "error_in_read_frame" << endl;
+			exit(EXIT_FAILURE);
+		}
+
+		//	GaussianBlur( frame, frame, Size( 3, 3 ), 0, 0 );
+		backGroundSubMOG2->operator ()(frame, fgMaskMOG2);
+
+		imshow("FG Mask MOG 2", fgMaskMOG2);
+		erode(fgMaskMOG2, fgMaskMOG2, kernelErosion); //Applico Erosione
+		dilate(fgMaskMOG2, fgMaskMOG2, kernelDilatation); //Applico Dilatazione
+		imshow("FG Mask MOG 2 Filtered", fgMaskMOG2);
+
+		vector<vector<Point> > contours;
+		//trova tutti i contorni dei BLOBS
+		findContours(fgMaskMOG2, contours, RETR_TREE, CHAIN_APPROX_SIMPLE,
+				Point(0, 0));
+		Mat drawing = Mat::zeros(fgMaskMOG2.size(), CV_8UC3);
+
+		/// Approximate contours to polygons + get bounding rects and circles
+		vector<vector<Point> > contours_poly(contours.size());
+		vector<Point2f> center(contours.size());
+		vector<float> radius(contours.size());
+
+		for (size_t i = 0; i < contours.size(); i++) {
+			Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255),
+					rng.uniform(0, 255));
+
+			///funzioni per calcolare il centro dei blob
+			approxPolyDP(Mat(contours[i]), contours_poly[i], 3, true); //approssima il contorno in un polinomio
+			minEnclosingCircle((Mat) contours_poly[i], center[i], radius[i]);
+
+			cout << "Center : " << center[i] << endl;
+			cout << "Area : " << contourArea(contours[i]) << endl;
+
+			if (contourArea(contours[i]) > MINAREA) //FUNZIONE per calcolare l'area dei BLOBs
+				drawContours(drawing, contours, (int) i, color, 2, 8, noArray(),
+						0, Point());
+
+		}
+
+		MatrixSimilarity m(3, 3);
+		m.matrix[0][0]=2;m.matrix[0][1]=1;m.matrix[0][2]=1.3;
+		m.matrix[1][0]=4;m.matrix[1][1]=2.1;m.matrix[1][2]=1.1;
+		cout<<"BLOB: "<<m.remainBlobs().size()<<endl;
+		cout<<"Obj: "<<m.remainObjects().size()<<endl;
+		cout<<m<<endl;
+
+		std::vector<float> max=m.maxMatrix();
+		cout<<max[0]<<" "<<max[1]<<" "<<max[2]<<endl;
+		m.deleteFromMatrix(max[0],max[1]);
+		cout<<"BLOB: "<<m.remainBlobs().size()<<endl;
+		cout<<"Obj: "<<m.remainObjects().size()<<endl;
+		cout<<"DOPO"<<endl;
+		cout<<m<<endl;
+
+
+		max=m.maxMatrix();
+		cout<<max[0]<<" "<<max[1]<<" "<<max[2]<<endl;
+		m.deleteFromMatrix(max[0],max[1]);
+		cout<<"BLOB: "<<m.remainBlobs().size()<<endl;
+		cout<<"Obj: "<<m.remainObjects().size()<<endl;
+		cout<<"DOPO"<<endl;
+		cout<<m<<endl<<endl;
+
+
+		cout<<"BLOB: "<<m.remainBlobs().size()<<endl;
+		cout<<"Obj: "<<m.remainObjects().size()<<endl;
+
+
+
+
+		//m.~MatrixSimilarity();
+
+		imshow("FG Mask MOG 2 blobs", drawing);
+		imshow("Frame", frame);
+		waitKey();
+
+		keyboard = waitKey(30);
+
+	}
+
+}
+
