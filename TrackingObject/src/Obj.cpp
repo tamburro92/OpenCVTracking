ebeac6bb4e7ebb9046604df342510a2276c2c866
ciro <cmiranda@hotmail.it>
2016-05-09 18:06:53 +0200

    Codice

---
+++ TrackingObject/src/Obj.cpp
@@ -0,0 +1,35 @@
+#include "Obj.hpp"
+
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/video/video.hpp>
+
+using namespace cv;
+using namespace std;
+using std::vector;
+
+Obj::Obj(int name) :
+		name(name) {
+	ghostFrame = 0;
+	blob.clear();
+}
+
+void Obj::associateBlob(vector<Point> b) {
+	blob = b;
+}
+
+void Obj::deassociateBlob(){
+	blob.clear();
+}
+
+int Obj::incremGhostFrame() {
+	ghostFrame++;
+	return ghostFrame;
+}
+
+int Obj::resetGhostFrame() {
+	ghostFrame = 0;
+	return ghostFrame;
+}
---
+++ TrackingObject/src/Obj.hpp
@@ -0,0 +1,49 @@
+#ifndef OBJ_HPP_
+#define OBJ_HPP_
+
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/video/video.hpp>
+
+using namespace cv;
+using namespace std;
+using std::vector;
+
+class Obj {
+private:
+	vector<Point> blob;
+	int name;
+	int ghostFrame;
+public:
+	/**
+	 * Costruttore
+	 * name: valore intero che identifica l'ID dell'oggetto
+	 */
+	Obj(int name);
+
+	/**
+	 * Associa all'oggetto il blob b
+	 */
+	void associateBlob(vector<Point> b);
+
+	/**
+	 * Deassocia il blob dal frame
+	 */
+	void deassociateBlob();
+
+	/**
+	 * Incrementa il ghost frame
+	 * Ritorna il valore del ghost frame dopo l'incremento
+	 */
+	int incremGhostFrame();
+
+	/**
+	 * Resetta il ghost frame, riportandolo a zero
+	 * Ritorna il valore del ghost frame a zero
+	 */
+	int resetGhostFrame();
+};
+
+#endif
